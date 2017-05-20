3a0647dc9e81f47f1740f23def0cfee8daaaf40d
ivofugers <i.fugers@gmail.com>
2017-05-20 11:37:15 +0200

    .

--- GLMPoissonLasso.R
+++ GLMPoissonLasso.R
@@ -3,137 +3,23 @@
 rm(list = ls())
 
 df_tycoon <- read.csv("Data//1. Modelling challenge//trainingset.csv")
+df_tycoon$coast_length[is.na(df_tycoon$coast_length)] <- 0
 
 library(glmmLasso); library(dplyr)
 library(MASS);library(nlme)
+
 test_tycoon <- as.character(unique(df_tycoon$typhoon_name)[1])
+df_tycoon[,5:38]<-scale(df_tycoon[,5:38],center=T,scale=T)
 df_train <- filter(df_tycoon, !typhoon_name==test_tycoon)
 df_test <- filter(df_tycoon, typhoon_name==test_tycoon)
 
+### Model for completely destroyed:
+df_train_comp <- select(df_train, -admin_L3_name, -part_damage_houses, -total_damage_houses)
+
 ## generalized additive mixed model
 ## grid for the smoothing parameter
-
-## center all metric variables so that also the starting values with glmmPQL are in the correct scaling
-#soccer[,c(4,5,9:16)]<-scale(soccer[,c(4,5,9:16)],center=T,scale=T)
-#soccer<-data.frame(soccer)
-
-
 lambda <- seq(500,0,by=-5)
 family = poisson(link = log)
-
-################## First Simple Method ############################################
-## Using BIC (or AIC, respectively) to determine the optimal tuning parameter lambda
-
-BIC_vec<-rep(Inf,length(lambda))
-
-## first fit good starting model
-
-PQL<-glmmPQL(points~1,random = ~1|team,family=family,data=soccer)
-Delta.start<-c(as.numeric(PQL$coef$fixed),rep(0,6),as.numeric(t(PQL$coef$random$team)))
-Q.start<-as.numeric(VarCorr(PQL)[1,1])
-
-for(j in 1:length(lambda))
-{
-  print(paste("Iteration ", j,sep=""))
-  
-  glm1 <- try(glmmLasso(points~transfer.spendings  
-                        + ave.unfair.score + ball.possession
-                        + tackles + ave.attend + sold.out, rnd = list(team=~1),  
-                        family = family, data = soccer, lambda=lambda[j],switch.NR=T,final.re=TRUE,
-                        control=list(start=Delta.start,q_start=Q.start)), silent=TRUE)  
-  
-  
-  if(class(glm1)!="try-error")
-  {  
-    BIC_vec[j]<-glm1$bic
-  }
-  
-}
-
-opt<-which.min(BIC_vec)
-
-glm1_final <- glmmLasso(points~transfer.spendings  
-                        + ave.unfair.score + ball.possession
-                        + tackles + ave.attend + sold.out, rnd = list(team=~1),  
-                        family = family, data = soccer, lambda=lambda[opt],switch.NR=F,final.re=TRUE,
-                        control=list(start=Delta.start,q_start=Q.start))
-
-
-
-summary(glm1_final)
-
-
-
-
-
-################## Second Simple Method ###########################
-## Using 5-fold CV to determine the optimal tuning parameter lambda
-
-### set seed
-set.seed(123)
-N<-dim(soccer)[1]
-ind<-sample(N,N)
-lambda <- seq(500,0,by=-5)
-
-kk<-5
-nk <- floor(N/kk)
-
-Devianz_ma<-matrix(Inf,ncol=kk,nrow=length(lambda))
-
-## first fit good starting model
-library(MASS);library(nlme)
-PQL<-glmmPQL(points~1,random = ~1|team,family=family,data=soccer)
-Delta.start<-c(as.numeric(PQL$coef$fixed),rep(0,6),as.numeric(t(PQL$coef$random$team)))
-Q.start<-as.numeric(VarCorr(PQL)[1,1])
-
-
-for(j in 1:length(lambda))
-{
-  print(paste("Iteration ", j,sep=""))
-  
-  for (i in 1:kk)
-  {
-    if (i < kk)
-    {
-      indi <- ind[(i-1)*nk+(1:nk)]
-    }else{
-      indi <- ind[((i-1)*nk+1):N]
-    }
-    
-    soccer.train<-soccer[-indi,]
-    soccer.test<-soccer[indi,]
-    
-    glm2 <- try(glmmLasso(points~transfer.spendings  
-                          + ave.unfair.score  + ball.possession
-                          + tackles + ave.attend + sold.out, rnd = list(team=~1),  
-                          family = family, data = soccer.train, lambda=lambda[j],switch.NR=F,final.re=TRUE,
-                          control=list(start=Delta.start,q_start=Q.start))
-                ,silent=TRUE) 
-    
-    if(class(glm2)!="try-error")
-    {  
-      y.hat<-predict(glm2,soccer.test)    
-      
-      Devianz_ma[j,i]<-sum(family$dev.resids(soccer.test$points,y.hat,wt=rep(1,length(y.hat))))
-    }
-  }
-  print(sum(Devianz_ma[j,]))
-}
-
-Devianz_vec<-apply(Devianz_ma,1,sum)
-opt2<-which.min(Devianz_vec)
-
-
-glm2_final <- glmmLasso(points~transfer.spendings  
-                        + ave.unfair.score + ball.possession
-                        + tackles + ave.attend + sold.out, rnd = list(team=~1),  
-                        family = family, data = soccer, lambda=lambda[opt2],switch.NR=F,final.re=TRUE,
-                        control=list(start=Delta.start,q_start=Q.start))
-
-
-
-summary(glm2_final)
-
 
 ################## More Elegant Method ############################################
 ## Idea: start with big lambda and use the estimates of the previous fit (BUT: before
@@ -144,28 +30,20 @@ summary(glm2_final)
 ## Using BIC (or AIC, respectively) to determine the optimal tuning parameter lambda
 lambda <- seq(500,0,by=-5)
 
-
 BIC_vec<-rep(Inf,length(lambda))
 family = poisson(link = log)
 
 # specify starting values for the very first fit; pay attention that Delta.start has suitable length! 
-Delta.start<-as.matrix(t(rep(0,7+23)))
+Delta.start<-as.matrix(t(rep(0,36)))
 Q.start<-0.1  
 
-for(j in 1:length(lambda))
-{
+for(j in 1:length(lambda)){
   print(paste("Iteration ", j,sep=""))
   
-  glm3 <- glmmLasso(points~1 +transfer.spendings
-                    + ave.unfair.score 
-                    + tackles  
-                    + sold.out 
-                    + ball.possession
-                    + ave.attend
-                    ,rnd = list(team=~1),  
-                    family = family, data = soccer, 
-                    lambda=lambda[j], switch.NR=F,final.re=TRUE,
-                    control = list(start=Delta.start[j,],q_start=Q.start[j]))  
+  glm3 <- glmmLasso(glm_form,rnd = list(typhoon_name=~1), 
+                    family = family, data = df_train_comp, 
+                    lambda=lambda[j], switch.NR=T,final.re=T,
+                    control=list(start=Delta.start[j,],q_start=Q.start[j]))  
   
   print(colnames(glm3$Deltamatrix)[2:7][glm3$Deltamatrix[glm3$conv.step,2:7]!=0])
   BIC_vec[j]<-glm3$bic
@@ -173,23 +51,25 @@ for(j in 1:length(lambda))
   Q.start<-c(Q.start,glm3$Q_long[[glm3$conv.step+1]])
 }
 
+plot(lambda, BIC_vec)
 opt3<-which.min(BIC_vec)
 
-glm3_final <- glmmLasso(points~transfer.spendings + ave.unfair.score 
-                        + ball.possession
-                        + tackles + ave.attend + sold.out, rnd = list(team=~1),  
-                        family = family, data = soccer, lambda=lambda[opt3],
-                        switch.NR=F,final.re=TRUE,
-                        control = list(start=Delta.start[opt3,],q_start=Q.start[opt3]))  
-
+glm3_final <- glmmLasso(glm_form, rnd = list(typhoon_name=~1),  
+                       family = family, data = df_train_comp, lambda=lambda[opt3])
 
 summary(glm3_final)
 
+predicted <- predict(glm3_final, newdata = df_test)
+
+plot(df_test$comp_damage_houses, predicted)
+cor(df_test$comp_damage_houses, predicted)
+
+
 ## plot coefficient paths
 par(mar=c(6,6,4,4))
-plot(lambda,Delta.start[2:(length(lambda)+1),2],type="l",ylim=c(-1e-1,1e-1),ylab=expression(hat(beta[j])))
+plot(lambda,Delta.start[2:(length(lambda)+1),2],ylim=c(-1,2),type="l",ylab=expression(hat(beta[j])))
 lines(c(-1000,1000),c(0,0),lty=2)
-for(i in 3:7){
+for(i in 3:36){
   lines(lambda[1:length(lambda)],Delta.start[2:(length(lambda)+1),i])
 }
 abline(v=lambda[opt3],lty=2)
