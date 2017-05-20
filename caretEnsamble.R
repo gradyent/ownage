7145b10573368f9b54f1c34a7249ffa7bcf466a1
Macaw <ietsadfs@gmail.com>
2017-05-20 12:30:38 +0000

    no message

--- caretEnsamble.R
+++ caretEnsamble.R
@@ -10,10 +10,15 @@ library(caTools)
 df_tycoon <- read.csv("Data//1. Modelling challenge//trainingset.csv")
 df_tycoon$coast_length[is.na(df_tycoon$coast_length)] <- 0
 
+df_tycoonTest <- read.csv("Data//1. Modelling challenge//testset.csv")
+df_tycoonTest$coast_length[is.na(df_tycoonTest$coast_length)] <- 0
 
-test_tycoon <- as.character(unique(df_tycoon$typhoon_name)[1])
-df_train <- filter(df_tycoon, !typhoon_name==test_tycoon)
-df_test <- filter(df_tycoon, typhoon_name==test_tycoon)
+#test_tycoon <- as.character(unique(df_tycoon$typhoon_name)[1])
+# df_train <- filter(df_tycoon, !typhoon_name==test_tycoon)
+# df_test <- filter(df_tycoon, typhoon_name==test_tycoon)
+df_train <- df_tycoon
+df_test <- df_tycoonTest
+
 
 df_test<- df_test[,c(5,8:38)]
 df_train<- df_train[,c(5,8:38)]
@@ -39,8 +44,6 @@ all.models <- caretList(df_train[-1], df_train$comp_damage_houses,metric = "Rsqu
   model5 <- caretModelSpec( method='knn', preProcess=PP),
   model6 <- caretModelSpec( method='earth', preProcess=PP),
   model7 <- caretModelSpec( method='glm',  preProcess=PP),
-  model8 <- caretModelSpec( method='svmRadial', preProcess=PP),
-  #model9 <- caretModelSpec( method='gam', preProcess=PP)
   model10 <- caretModelSpec( method='glmnet', preProcess=PP)
 ))
 
@@ -57,9 +60,7 @@ greedy_ensemble <- caretEnsemble(
   ))
 summary(greedy_ensemble)
 
-
 stopCluster(cl)
-
 
 model_preds <- lapply(all.models, predict, newdata=df_test, type="raw")
 model_preds <- lapply(model_preds, function(x) x[,"M"])
